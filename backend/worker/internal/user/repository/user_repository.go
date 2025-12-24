aa622c3deeb8c63a069cf484902ed8dbbe45cb46
Rafael Carneiro <rafael.cr.carneiro@gmail.com>
2025-12-24 17:49:24 -0300

    feat: adding implementation of repo and service interfaces

---
+++ backend/worker/internal/user/repository/user_repository.go
@@ -0,0 +1,113 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/rafaeldepontes/worker-pool-design/internal/user"
+	"github.com/rafaeldepontes/worker-pool-design/internal/user/model"
+	"github.com/rafaeldepontes/worker-pool-design/pkg/database/postgres"
+)
+
+var (
+	createStmt *sql.Stmt
+	updateStmt *sql.Stmt
+	deleteStmt *sql.Stmt
+)
+
+type userRepository struct {
+	db *sql.DB
+}
+
+func NewRepository() user.Repository {
+	ur := userRepository{
+		db: postgres.GetDb(),
+	}
+
+	createStmt, _ = ur.preparedInsertStatement()
+	updateStmt, _ = ur.preparedUpdateStatement()
+	deleteStmt, _ = ur.preparedDeleteStatement()
+
+	return &ur
+}
+
+func (ur userRepository) Create(user *model.User) error {
+	stmt, _ := ur.preparedInsertStatement()
+
+	_, err := stmt.Exec(user.Username, user.Age)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ur userRepository) Update(newUser *model.User) error {
+	stmt, _ := ur.preparedInsertStatement()
+
+	_, err := stmt.Exec(newUser.ID, newUser.Username, newUser.Age)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ur userRepository) Delete(id int64) error {
+	stmt, _ := ur.preparedInsertStatement()
+
+	_, err := stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CloseStatements() error {
+	if err := createStmt.Close(); err != nil {
+		return err
+	}
+
+	if err := updateStmt.Close(); err != nil {
+		return err
+	}
+
+	if err := deleteStmt.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ur userRepository) preparedInsertStatement() (*sql.Stmt, error) {
+	stmt, err := ur.db.Prepare(`insert into users (username, age) values ($1, $2)`)
+	if err != nil {
+		fmt.Println("[ERROR] something went wrong trying to prepare the insert sql\n", err)
+		return nil, err
+	}
+	return stmt, nil
+}
+
+func (ur userRepository) preparedUpdateStatement() (*sql.Stmt, error) {
+	stmt, err := ur.db.Prepare(`
+		update users
+		set username = $2,
+		age = $3
+		where id = $1
+	`)
+	if err != nil {
+		fmt.Println("[ERROR] something went wrong trying to prepare the update sql\n", err)
+		return nil, err
+	}
+	return stmt, nil
+}
+
+func (ur userRepository) preparedDeleteStatement() (*sql.Stmt, error) {
+	stmt, err := ur.db.Prepare(`delete from users where id = $1`)
+	if err != nil {
+		fmt.Println("[ERROR] something went wrong trying to prepare the delete sql\n", err)
+		return nil, err
+	}
+	return stmt, nil
+}
---
+++ backend/worker/internal/user/service/user_service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"github.com/rafaeldepontes/worker-pool-design/internal/process"
+	"github.com/rafaeldepontes/worker-pool-design/internal/user"
+	"github.com/rafaeldepontes/worker-pool-design/internal/user/model"
+	userRepo "github.com/rafaeldepontes/worker-pool-design/internal/user/repository"
+)
+
+type userService struct {
+	repo user.Repository
+}
+
+func NewService() user.Service {
+	return &userService{
+		repo: userRepo.NewRepository(),
+	}
+}
+
+func (us *userService) ProcessData(d process.Process[model.User]) error {
+	switch d.Type {
+	case "create":
+		println("creating users")
+		if err := us.repo.Create(&d.Data); err != nil {
+			return err
+		}
+	case "update":
+		println("updatings users")
+		if err := us.repo.Update(&d.Data); err != nil {
+			return err
+		}
+	case "delete":
+		println("deleting users")
+		if err := us.repo.Delete(d.Data.ID); err != nil {
+			return err
+		}
+	}
+	return nil
+}
