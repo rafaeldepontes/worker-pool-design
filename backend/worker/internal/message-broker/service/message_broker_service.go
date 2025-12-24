6c1eb551247dda37d58dcf2ef14258e24a447faa
Rafael Carneiro <rafael.cr.carneiro@gmail.com>
2025-12-24 17:48:43 -0300

    refactor: adding a new service layer to the worker

--- backend/worker/internal/message-broker/service/message_broker_service.go
+++ backend/worker/internal/message-broker/service/message_broker_service.go
@@ -1,25 +1,28 @@
 package service
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
 	"time"
 
 	messagebroker "github.com/rafaeldepontes/worker-pool-design/internal/message-broker"
-	"github.com/rafaeldepontes/worker-pool-design/pkg/database/postgres"
+	"github.com/rafaeldepontes/worker-pool-design/internal/process"
+	"github.com/rafaeldepontes/worker-pool-design/internal/user"
+	"github.com/rafaeldepontes/worker-pool-design/internal/user/model"
+	uSvc "github.com/rafaeldepontes/worker-pool-design/internal/user/service"
 )
 
 const BrazilianDateTimeFormat = "02/01/2006 15:04:05"
 
 type messageService struct {
-	db     *sql.DB
-	rmqSvc *rabbitMQService
+	userSvc user.Service
+	rmqSvc  *rabbitMQService
 }
 
 func NewService() messagebroker.Service {
 	return &messageService{
-		db:     postgres.GetDb(),
-		rmqSvc: newRabbitMQService(),
+		userSvc: uSvc.NewService(),
+		rmqSvc:  newRabbitMQService(),
 	}
 }
 
@@ -27,9 +30,19 @@ func NewService() messagebroker.Service {
 func (msgSvc *messageService) Worker(id int) {
 	fmt.Printf("Worker %d started\n", id)
 	var s *string
+
 	for data := range *msgSvc.rmqSvc.consumer {
+		var processModel process.Process[model.User]
+		if err := json.Unmarshal(data.Body, &processModel); err != nil {
+			fmt.Println("[ERROR] unexpected behaviour while unmarshalling the data\n", err)
+			// RETRY MECHANISM
+			continue
+		}
+
 		s = strPtr(time.Now().Local().Format(BrazilianDateTimeFormat))
-		fmt.Printf("%s: Worker %d processing: %s\n", *s, id, string(data.Body))
+		fmt.Printf("%s: Worker %d processing: %s\n", *s, id, processModel.Type)
+
+		msgSvc.userSvc.ProcessData(processModel)
 	}
 }
 
